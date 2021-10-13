6d8a65c8203f54e715af490271aa90be2eec8e0c
raydan4 <dszafran116@gmail.com>
2021-10-13 01:06:37 -0400

    Added docker stuff, untested

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 from os import environ
 
 BALLOT_BOX_URL = environ.get("BALLOT_BOX_URL", "http://localhost:5001")
-DATABASE_URL = environ.get("DATABASE_URL", "sqlite:///.sqlite.db")
+DATABASE_URL = environ.get("DATABASE_URL", "sqlite:///data/sqlite.db")
 RESULTSERVER_URL = environ.get("RESULTSERVER_URL", "http://localhost:5002")
